f495dfc2dd1842017badc04b090cc7e7195707f1
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2023-12-19 12:51:54 +0900

    2023/12/19

--- CSharp-Programing/00. Programing/obj/Debug/net8.0/00. Programing.AssemblyInfo.cs
+++ CSharp-Programing/00. Programing/obj/Debug/net8.0/00. Programing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("00. Programing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e53a2539af46bf1014e016fea281010c6a659232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("00. Programing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("00. Programing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CSharp-Programing/01. Variable/obj/Debug/net8.0/01. Variable.AssemblyInfo.cs
+++ CSharp-Programing/01. Variable/obj/Debug/net8.0/01. Variable.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01. Variable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e53a2539af46bf1014e016fea281010c6a659232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("01. Variable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01. Variable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
