8cb1d95cc332961a64c82644443aded098dbcfda
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2023-12-20 11:45:44 +0900

    2023/12/20

--- CSharp-Programing/02. Operator/obj/Debug/net8.0/02. Operator.AssemblyInfo.cs
+++ CSharp-Programing/02. Operator/obj/Debug/net8.0/02. Operator.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("01. Variable")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("02. Operator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("01. Variable")]
-[assembly: System.Reflection.AssemblyTitleAttribute("01. Variable")]
+[assembly: System.Reflection.AssemblyProductAttribute("02. Operator")]
+[assembly: System.Reflection.AssemblyTitleAttribute("02. Operator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
