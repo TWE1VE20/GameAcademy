ad39884e5ceaf35415dd3254be79a1c8318f0f86
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2023-12-21 14:56:10 +0900

    2023/12/21

---
+++ 2023_12_21/Loop_Star/Loop_Star/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Loop_Star
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StarLoop1();
+            StarLoop2();
+            StarLoop3();
+            StarLoop4();
+            StarLoop5();
+        }
+
+        static void StarLoop1()
+        {
+            Console.Write("숫자를 입력하세요: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+        }
+
+        static void StarLoop2()
+        {
+            Console.Write("숫자를 입력하세요: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+
+        static void StarLoop3() {
+            Console.Write("숫자를 입력하세요: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+
+        static void StarLoop4()
+        {
+            Console.Write("숫자를 입력하세요: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num - i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+
+        static void StarLoop5()
+        {
+            Console.Write("숫자를 입력하세요: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num - i; j++)
+                    Console.Write(" ");
+                for (int j = 0; j <= i*2; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            for (int i = num; i >= 0; i--)
+            {
+                for (int j = 0; j < num - i; j++)
+                    Console.Write(" ");
+                for (int j = 0; j <= i * 2; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+    }
+
+}
