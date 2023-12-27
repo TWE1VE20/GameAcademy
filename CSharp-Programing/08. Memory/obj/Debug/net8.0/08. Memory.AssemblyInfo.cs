1e7f0802435e61f340f66aa3e1438ad4d70c7e2a
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2023-12-27 16:16:55 +0900

    2023/12/27

--- CSharp-Programing/08. Memory/obj/Debug/net8.0/08. Memory.AssemblyInfo.cs
+++ CSharp-Programing/08. Memory/obj/Debug/net8.0/08. Memory.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("06. UserDefineType")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("08. Memory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("06. UserDefineType")]
-[assembly: System.Reflection.AssemblyTitleAttribute("06. UserDefineType")]
+[assembly: System.Reflection.AssemblyProductAttribute("08. Memory")]
+[assembly: System.Reflection.AssemblyTitleAttribute("08. Memory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
