9daf0e91805d267a586e1b895d092e0151ccb564
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-09 10:41:34 +0900

    2024/01/09

--- GitTest/GitTest/GitTest/Program.cs
+++ GitTest/GitTest/GitTest/Program.cs
@@ -2,9 +2,11 @@
 {
     internal class Program
     {
+        public int hp;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");     // 설명 추가
         }
     }
 }
