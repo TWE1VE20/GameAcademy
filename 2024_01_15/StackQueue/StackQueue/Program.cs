608c44a9115fb03675a0d2c396d5f8af99bcc66b
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-15 15:50:54 +0900

    feat : stack 와 queue 과제 추가

--- 2024_01_15/StackQueue/StackQueue/Program.cs
+++ 2024_01_15/StackQueue/StackQueue/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace StackQueue
+﻿using System.Linq;
+
+namespace StackQueue
 {
     internal class Program
     {
@@ -9,32 +11,49 @@
             Stack<char> stack1 = new Stack<char>();
             foreach (char str in check)
             {
-                switch (str) 
+                switch (str)
                 {
                     case '(':
                     case '[':
                     case '{':
+                        stack1.Push(str);
+                        break;
                     case ')':
+                        if (stack1.Count != 0 && stack1.Pop() != '(') return false;
+                        break;
                     case ']':
+                        if (stack1.Count != 0 && stack1.Pop() != '[') return false;
+                        break;
                     case '}':
+                        if (stack1.Count != 0 && stack1.Pop() != '{') return false;
+                        break;
                     default:
                         break;
                 }
             }
-
-            for (int i = 0; i < stack1.Count; i++)
-                switch (stack1.Pop())
-                {
-                    case '(':
-                        if (stack1.Pop() != ')') return false; break;
-                    case '[':
-                        if (stack1.Pop() != ']') return false; break;
-                    case '{':
-                        if (stack1.Pop() != '}') return false; break;
-                    default:
-                        return false;
-                }
             return true;
+        }
+
+        static public int[] Process(int[] check) {
+            Queue<int> queue = new Queue<int>();
+            List<int> list = new List<int>();
+            int buf = 0; int work = -1;
+            foreach (int time in check)
+                queue.Enqueue(time);
+            while (true) {
+                for (int quota = 0; quota < 8; quota++) 
+                {
+                    if (buf == 0)
+                    {
+                        work += 1;
+                        if (queue.Count == 0) 
+                            return list.ToArray();
+                        buf = queue.Dequeue();
+                    }
+                    buf -= 1;
+                }
+                list.Add(work);
+            }
         }
 
         static void Main(string[] args)
@@ -43,8 +62,16 @@
             Console.WriteLine("[]{}()를 이용해 입력해주세요");
             string input = Console.ReadLine();
 
-            if (BracketChecker(input)) { Console.WriteLine("완성되어 있습니다."); return; }
-            Console.WriteLine("미완성입니다.");
+            if (BracketChecker(input)) Console.WriteLine("완성되어 있습니다.");
+            else Console.WriteLine("미완성입니다.");
+
+            List<int> schedule = new List<int>();
+
+            Console.WriteLine("스캐줄을 입력해주세요");
+            input = Console.ReadLine();
+            foreach (string ints in input.Split(" "))
+                schedule.Add(int.Parse(ints));
+            Console.Write(string.Join(", ", Process(schedule.ToArray())));
         }
     }
 }
