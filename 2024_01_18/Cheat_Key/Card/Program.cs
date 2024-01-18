fb04fd2aea39e0c146b380f81d43e994d00a97f5
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-18 17:55:04 +0900

    feat : 실습 과제 추가

---
+++ 2024_01_18/Cheat_Key/Card/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Card
+{
+    internal class Program
+    {
+        static void Buildnum(int loop, List<int> ints, string num, HashSet<string> Hset)
+        {
+            if (loop-1 == 0)
+            {
+                if(!Hset.Contains(num))
+                    Hset.Add(num);
+                return;
+            }
+            for(int i = 0; i < ints.Count; i++)
+            {
+                List<int> inputint = ints.ToList();
+                int this_num = inputint[i];
+                inputint.RemoveAt(i);
+                Buildnum(loop - 1, inputint, num + (Convert.ToString(this_num)), Hset);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            HashSet<string> Hset = new HashSet<string>();
+            string input;
+            List<int> Cards = new List<int>();
+
+            input = Console.ReadLine();
+            int n = int.Parse(input);
+            input = Console.ReadLine();
+            int k = int.Parse(input);
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine();
+                Cards.Add( int.Parse(input));
+            }
+            for (int i = 0; i < Cards.Count; i++)
+            {
+                List<int> inputint = Cards.ToList();
+                int this_num = inputint[i];
+                inputint.RemoveAt(i);
+                Buildnum(k, inputint, Convert.ToString(this_num), Hset);
+            }
+            Console.WriteLine(Hset.Count);
+        }
+    }
+}
---
+++ 2024_01_18/Cheat_Key/FindPassword/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace FindPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int input_num = int.Parse((input.Split(" "))[0]);
+            Dictionary<string, string> Password = new Dictionary<string, string>(input_num);
+            int pass_num = int.Parse(input);
+            string output;
+                
+            for (int i = 0; i < input_num; i++)
+            {
+                input = Console.ReadLine();
+                Password.Add(input.Split(" ")[0], input.Split(" ")[1]);
+            }
+
+            for (int i = 0; i < pass_num; i++)
+            {
+                input = Console.ReadLine();
+                Password.TryGetValue(input, out output);
+                Console.WriteLine(output);
+            }
+
+        }
+    }
+}
