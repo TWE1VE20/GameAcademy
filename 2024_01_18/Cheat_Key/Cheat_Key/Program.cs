f9f964696163a753acbcbe23811c43124ae90610
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-18 15:47:36 +0900

    feat : 실습 과제 추가

---
+++ 2024_01_18/Cheat_Key/Cheat_Key/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Cheat_Key
+{
+    internal class Program
+    {
+        public class CheatKey
+        {
+            private Dictionary<string, Action> cheatDic = new Dictionary<string, Action>(2);
+
+            public CheatKey()
+            {
+                cheatDic.Add("ShowMeTheMoney", ShowMeTheMoney);
+                cheatDic.Add("ThereIsNoCowLevel", ThereIsNoCowLevel);
+            }
+
+            public void Run(string cheatkey)
+            {
+                if (cheatDic.TryGetValue(cheatkey, out Action cheat))
+                    cheat.Invoke();
+            }
+
+            public void ShowMeTheMoney()
+            {
+                Console.WriteLine("골드를 늘려주는 치트키 발동!");
+            }
+
+            public void ThereIsNoCowLevel()
+            {
+                Console.WriteLine("바로 승리하는 치트키 발동!");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            CheatKey Cheats = new CheatKey();
+            string? input;
+
+            while (true)
+            {
+                Console.Write("코드를 입력해보거나 (E로 Exit) : ");
+                input = Console.ReadLine();
+                if (input == "e" || input == "E")
+                    Environment.Exit(0);
+                else if (input != null)
+                    Cheats.Run(input);
+            }
+        }
+    }
+}
