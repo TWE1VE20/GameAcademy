d15223c80312dbe654d32695c54ada6cd83fc987
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-19 14:46:33 +0900

    feat : DesignTechnique 추가
    재귀, 분할정복, 동적계획법 백트래킹, 탐욕 알고리즘 추가

---
+++ 2024_01_19/DesignTechnique/DesignTechnique/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignTechnique
+{
+    internal class NANDM
+    {
+        static void NandM(int N, int M, List<int> print) 
+        {
+            if (M == 0)
+            {
+                Console.WriteLine(string.Join(",", print));
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < N; i++) 
+                {
+                    List<int> printist;
+                    if (print == null) printist = new List<int>();
+                    else printist = print.ToList();
+                    printist.Add(i+1);
+                    NandM(N, M - 1, printist);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            NandM(int.Parse(input.Split(" ")[0]),int.Parse(input.Split(" ")[1]), null);
+        }
+    }
+}
